1b407258f44e0c84343a91fd8e9676b1fd2a0c44
Ben Fulton <ben@benfulton.net>
2012-04-27 23:47:32 -0400

    Remove some styling that belonged in the layout, and clean up the layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,7 @@ class User < ActiveRecord::Base
   has_many :authorizations, :dependent => :destroy
 
   # Setup accessible (or protected) attributes for your model
-<<<<<<< HEAD
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :userlijsts
-=======
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :authorizations
->>>>>>> 82e3b88f6d345d0ed360be669cc86cb9e7fbf572
 end
